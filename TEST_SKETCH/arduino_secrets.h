11060802a07c972dd5620170dd01ddc348ee2f6a
Alexander Entinger <cto@lxrobotics.com>
2020-02-19 08:23:27 +0100

    Adding code for supporting CL-GSM-1400-PROD-THING with CL-GSM-1400-PROD board

--- TEST_SKETCH/arduino_secrets.h
+++ TEST_SKETCH/arduino_secrets.h
@@ -1,2 +1,7 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
+#define SECRET_SSID       ""
+#define SECRET_PASS       ""
+
+#define SECRET_APN        ""
+#define SECRET_PIN        ""
+#define SECRET_USERNAME   ""
+#define SECRET_PASSWORD   ""
--- TEST_SKETCH/thingProperties.h
+++ TEST_SKETCH/thingProperties.h
@@ -16,6 +16,8 @@
 const char THING_ID[] = "2cfad7de-ae22-47c8-b0c4-a95db6b5572c"; /* CL-WIFI-1010-PROD */
 #elif defined(ARDUINO_SAMD_MKR1000)
 const char THING_ID[] = "71eb0f3a-2212-4b67-86d4-07d957e9f3cb"; /* CL-1000-PROD */
+#elif defined(ARDUINO_SAMD_MKRGSM1400)
+const char THING_ID[] = "a51e12e4-ee51-4b9e-907b-47b70fc80726"; /* CL-GSM-1400-PROD */
 #endif
 
 /******************************************************************************
@@ -45,6 +47,8 @@ void initProperties()
 
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKR1000)
 WiFiConnectionHandler ArduinoIoTPreferredConnection(SECRET_SSID, SECRET_PASS);
+#elif defined(ARDUINO_SAMD_MKRGSM1400)
+GSMConnectionHandler ArduinoIoTPreferredConnection(SECRET_PIN, SECRET_APN, SECRET_USERNAME, SECRET_PASSWORD);
 #endif
 
 #endif /* THING_PROPERTIES_H_ */
