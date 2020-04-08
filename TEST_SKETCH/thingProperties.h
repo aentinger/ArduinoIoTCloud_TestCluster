0d3d5f557947ff59caa81e244ef5eca9d6c03b07
Alexander Entinger <cto@lxrobotics.com>
2020-04-08 13:11:30 +0200

    Adding Nano 33 IoT thing

--- TEST_SKETCH/thingProperties.h
+++ TEST_SKETCH/thingProperties.h
@@ -18,6 +18,9 @@ const char THING_ID[] = "2cfad7de-ae22-47c8-b0c4-a95db6b5572c";
 #elif defined(ARDUINO_SAMD_MKR1000)
 /* CL-1000-PROD-THING */
 const char THING_ID[] = "71eb0f3a-2212-4b67-86d4-07d957e9f3cb";
+#elif defined(ARDUINO_SAMD_NANO_33_IOT)
+/* CL-NANO-33-IOT-PROD-THING */
+const char THING_ID[] = "05664e84-f6b0-4cea-9784-56f6d9f04dfe";
 #elif defined(ARDUINO_SAMD_MKRGSM1400)
 /* CL-GSM-1400-PROD-THING */
 const char THING_ID[] = "a51e12e4-ee51-4b9e-907b-47b70fc80726";
@@ -59,10 +62,10 @@ void initProperties()
 #endif
   ArduinoCloud.setThingId(THING_ID);
   ArduinoCloud.addProperty(led, READWRITE, ON_CHANGE, onLedChange);
-  ArduinoCloud.addProperty(seconds, READWRITE, 5 * SECONDS, onSecondsChange);
+  ArduinoCloud.addProperty(seconds, READ, ON_CHANGE, onSecondsChange);
 }
 
-#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKR1000) || defined(BOARD_ESP8266)
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(BOARD_ESP8266)
 WiFiConnectionHandler ArduinoIoTPreferredConnection(SECRET_SSID, SECRET_PASS);
 #elif defined(ARDUINO_SAMD_MKRGSM1400)
 GSMConnectionHandler ArduinoIoTPreferredConnection(SECRET_PIN, SECRET_APN, SECRET_USERNAME, SECRET_PASSWORD);
